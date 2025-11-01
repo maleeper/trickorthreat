758fdda0be48e3e02cc8d012528ed7910d0e3afc
sthDINESH <sthapit.dnsh@gmail.com>
2025-11-01 16:48:36 +0000

    fix issue with url - missing trailing slash

--- phish_buster/urls.py
+++ phish_buster/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('quiz', views.quiz, name='quiz'),
+    path('quiz/', views.quiz, name='quiz'),
     path('quiz/<int:session_id>/', views.quiz, name='quiz_with_session_id'),
     path('', views.home, name='home'),
 ]
